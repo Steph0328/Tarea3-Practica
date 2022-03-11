c30d845fc053cf59337890835b54b3137a7c130f
smont <smont@DESKTOP-UD4B600>
2022-03-10 18:43:18 -0600

    Se agrega el dao, y service tambien se crea la base de datos

---
+++ Tarea3/src/main/java/com/Tarea3/dao/BolsoDao.java
@@ -0,0 +1,8 @@
+package com.tarea3.dao;
+import com.tarea3.domain.Bolso;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface BolsoDao extends CrudRepository<Bolso, Long>{
+    
+}
